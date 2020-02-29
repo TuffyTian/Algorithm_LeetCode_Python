b8832dd6ae29b868b4639818cfd2defd6cec00b3
TuffyTian <tiantengfei@outlook.com>
2020-03-01 04:27:30 +0900

    two num

---
+++ 1_two_sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoNum(self, nums, target):
+        tmp = {}
+        n = len(nums)
+        for i in range(0, n):
+            num = target - nums[i]
+            if num not in tmp:
+                tmp[nums[i]] = i
+            else:
+                return [tmp[num], i]
+
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    print(Solution().twoNum(nums, target))
