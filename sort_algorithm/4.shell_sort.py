298efb92bbb4d4ce4c4102ace8995d4c3f27b13f
TuffyTian <tiantengfei@outlook.com>
2020-03-03 19:53:19 +0900

    add the shell sort algorithm

--- sort_algorithm/4.shell_sort.py
+++ sort_algorithm/4.shell_sort.py
@@ -8,17 +8,15 @@ class Resolution:
         while gaps > 0:
             for i in range(gaps, n):
                 j = i
-                temp = nums[j]
                 while j > gaps - 1 and nums[j] < nums[j - gaps]:
                     tem = nums[j]
                     nums[j] = nums[j - gaps]
                     nums[j - gaps] = tem
-                    j = j - 1
-                nums[j] = temp
+                    j = j - gaps
             gaps = (gaps - 1) // 3
         return nums
 
 
 if __name__ == "__main__":
-    nums = [1, 4, 6, 3, 9, 2]
+    nums = [5, 4, 6, 3, 9, 2, 10, 32, 33]
     print(Resolution().shell_sort(nums))
