85eb466f54d6ec6a11c950420f04a1ef210b5e17
TuffyTian <tiantengfei@outlook.com>
2020-04-07 02:18:45 +0900

    fix

--- sort_algorithm/4.shell_sort.py
+++ sort_algorithm/4.shell_sort.py
@@ -1,21 +1,21 @@
-def shell_sort(nums: [int]):
-    n = len(nums)
+def shell_sort(array: [int]):
+    n = len(array)
     gaps = 1
-    while gaps < (len(nums) // 3):
+    while gaps < (len(array) // 3):
         gaps = gaps * 3 + 1
 
     while gaps > 0:
         for i in range(gaps, n):
             j = i
-            while j > gaps - 1 and nums[j] < nums[j - gaps]:
-                tem = nums[j]
-                nums[j] = nums[j - gaps]
-                nums[j - gaps] = tem
+            while j > gaps - 1 and array[j] < array[j - gaps]:
+                tem = array[j]
+                array[j] = array[j - gaps]
+                array[j - gaps] = tem
                 j = j - gaps
         gaps = (gaps - 1) // 3
-    return nums
+    return array
 
 
 if __name__ == "__main__":
-     nums = [5, 4, 6, 3, 9, 2, 10, 32, 33]
+    nums = [5, 4, 6, 3, 9, 2, 10, 32, 33]
     print(shell_sort(nums))
